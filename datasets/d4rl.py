6dd45e06340ee4ebdbc5dd949c9608433cea89e4
jasmeetkaur9 <jasmeetkaur7642@gmail.com>
2025-08-27 18:11:40 +0000

    sac agent to collect d4rl env data

--- datasets/d4rl.py
+++ datasets/d4rl.py
@@ -3,10 +3,11 @@
 # sample goals 
 import os
 os.environ["MUJOCO_GL"] = "egl"
-import gymnasium as gym
+import gymnasium
 import gymnasium_robotics
+import numpy
 
-gym.register_envs(gymnasium_robotics)
+gymnasium.register_envs(gymnasium_robotics)
 import d4rl
 from torchrl.envs.libs.gym import GymEnv
 from torchrl.data.datasets.minari_data import MinariExperienceReplay
@@ -16,6 +17,7 @@ import minari
 from torchrl.data import ReplayBuffer
 from torchrl.data.replay_buffers import LazyMemmapStorage
 from stable_baselines3 import SAC
+from gymnasium.wrappers import FlattenObservation
 
 from minari import DataCollector
 
@@ -26,14 +28,58 @@ from minari import DataCollector
 # all_envs = list(gym.registry)
 # for env_id in sorted(all_envs):
 #     print(env_id)
-dataset_id = 'AntMaze_Medium-v4'
+# dataset_id = 'AntMaze_Medium-v4'
 
-env = DataCollector(gym.make(dataset_id, render_mode="rgb_array"))
-agent = SAC()
+# env = DataCollector(gym.make(dataset_id, render_mode="rgb_array"))
+# agent = SAC()
 
-dataset = env.create_dataset(
-    dataset_id = dataset_id,
-    algorithm_name = "SAC")
+# dataset = env.create_dataset(
+#     dataset_id = dataset_id,
+#     algorithm_name = "SAC")
 
-rb = ReplayBuffer(storage=LazyMemmapStorage(1000), batch_size=10)
-d = rb.sample()
+# rb = ReplayBuffer(storage=LazyMemmapStorage(1000), batch_size=10)
+# d = rb.sample()
+
+
+class D4RLCollector:
+    def __init__(self, env_id):
+        self.env_id = env_id 
+    
+    def collect_data(self):
+        self.env = DataCollector(gymnasium.make(self.env_id, render_mode="rgb_array")) # return a dict of observation and goals
+        
+        # Env for the Agent
+        self.env_agent = FlattenObservation(self.env)
+        self.agent = SAC("MlpPolicy", self.env_agent)
+        self.agent.learn(total_timesteps=10_000_000, log_interval=4)
+        self.agent.save("sac")
+        ep = 0
+        obs, info = self.env_agent.reset()
+        rewards = []
+        
+        while ep < 20000:
+            action, _states = self.agent.predict(obs, deterministic=True)
+
+            # take action in the datacollector env for storage
+            obs_d, reward_d, done_d, trucated_d, info_d = self.env.step(action) 
+
+            # take action using the agent
+            obs, reward, done, trucated, info = self.env_agent.step(action) 
+            rewards.append(reward)
+            if done or trucated:
+                print(numpy.mean(rewards))
+                obs, info = self.env_agent.reset()
+                _, _  = self.env.reset()
+                ep+=1
+                rewards = []
+
+        self.dataset = self.env.create_dataset(
+            dataset_id = "antmaze/sbsac-v1",
+        )
+
+        return self.dataset
+        
+
+
+collector = D4RLCollector(env_id = 'AntMaze_Medium-v4')
+collector.collect_data()
